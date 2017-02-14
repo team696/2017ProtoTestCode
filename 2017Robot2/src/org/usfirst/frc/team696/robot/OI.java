2e57766690c8e9e5f53756725a2a1e0128c40614
FRC696 <FRC696@FRC696-Prog1.gusd.lcl>
2017-02-13 17:57:40 -0800

    Working on DriveStraight and Locking Position

--- 2017Robot2/src/org/usfirst/frc/team696/robot/OI.java
+++ 2017Robot2/src/org/usfirst/frc/team696/robot/OI.java
@@ -5,6 +5,9 @@ import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 import org.usfirst.frc.team696.robot.commands.ExampleCommand;
+import org.usfirst.frc.team696.robot.commands.LockPosition;
+import org.usfirst.frc.team696.robot.commands.ToggleDriveStraight;
+import org.usfirst.frc.team696.robot.commands.ChangeTurnMultipler;
 import org.usfirst.frc.team696.robot.commands.ToggleIntake;
 
 /**
@@ -17,8 +20,19 @@ public class OI {
 	public static Joystick gamePad = new Joystick(2);
 	
 	JoystickButton runIntake = new JoystickButton(gamePad, 5);
+	JoystickButton fastTurn = new JoystickButton(wheel, 6);
+	JoystickButton toggleDriveStraight = new JoystickButton(wheel, 11);
+	JoystickButton lockPosition = new JoystickButton(wheel, 1);
 	
 	public OI(){
 		runIntake.whenActive(new ToggleIntake());
+		
+		fastTurn.whenPressed(new ChangeTurnMultipler(1.5));
+		fastTurn.whenReleased(new ChangeTurnMultipler(0.6));
+		
+		toggleDriveStraight.whenPressed(new ToggleDriveStraight());
+		
+		lockPosition.whenPressed(new LockPosition(0.04, 0.1, 0, 0.3));
+		lockPosition.whenReleased(new LockPosition(0.012, 0, 0, 0));
 	}
 }
