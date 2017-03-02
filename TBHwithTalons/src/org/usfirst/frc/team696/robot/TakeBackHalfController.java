f4e3f4f2f59eec0cfb7b9e64b603375bbe2e8c16
Daniel <disidrot@gmail.com>
2017-03-01 20:04:51 -0800

    Add TBH with multithreading.

---
+++ TBHwithTalons/src/org/usfirst/frc/team696/robot/TakeBackHalfController.java
@@ -0,0 +1,54 @@
+package org.usfirst.frc.team696.robot;
+
+public class TakeBackHalfController implements Runnable{
+
+	Thread t;
+	String threadName = "thread";
+	double error = 0;
+	double oldError = 10;
+	double targetRPM = 0;
+	double outputValue = 0;
+	double gain = 0;
+	double tbh = 0;
+
+	public TakeBackHalfController(double gain){
+		this.gain = gain;
+	}
+	
+	public void start(){
+		if(t == null)t = new Thread(this, threadName);
+		t.start();
+	}
+	
+	public void setGain(double gain){
+		this.gain = gain;
+	}
+	
+	public void setTargetRPM(double targetRPM){
+		this.targetRPM = targetRPM;
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		error = targetRPM - getRPM();
+		
+		outputValue += gain*error;
+		if(outputValue > 1)outputValue = 1;
+		if(outputValue < -1)outputValue = -1;
+		
+		if(oldError > 0 != error > 0){
+			outputValue = 0.5 * (outputValue + tbh);
+			tbh = outputValue;
+			oldError = error;
+		}
+	}
+
+	private double getRPM(){
+		return Robot.masterTalon.get();
+	}
+	
+	public double getOutputValue(){
+		return outputValue;
+	}
+}
